fa5ed3b85962a62b0cff80da7b649bab5383894f
rjbx <bob@coded.art>
2022-03-23 05:57:22 -0400

    Update repository ctor signature to only accept datasources in order to enforce decoupling of context from business logic

--- app/src/ktx/java/art/coded/wireframe/model/ElementRepository.kt
+++ app/src/ktx/java/art/coded/wireframe/model/ElementRepository.kt
@@ -1,5 +1,6 @@
 package art.coded.wireframe.model
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import art.coded.wireframe.model.local.ElementDao
 import art.coded.wireframe.model.local.ElementRoomDatabase
@@ -12,10 +13,11 @@ import kotlinx.coroutines.withContext
 
 private val LOG_TAG = ElementRepository::class.java.simpleName
 
-class ElementRepository(context: Context) {
-    private val mElementDao: ElementDao
+class ElementRepository(elementDao: ElementDao) {
 
-    val allElements: LiveData<List<Element>>
+    private val mElementDao: ElementDao = elementDao
+    val allElements: LiveData<List<Element>> = elementDao.all
+
     suspend fun insert(element: Element) { withContext(Dispatchers.IO) { mElementDao.insert(element) } }
     suspend fun delete(element: Element) { withContext(Dispatchers.IO) { mElementDao.delete(element) } }
     suspend fun deleteAll() { withContext(Dispatchers.IO) { mElementDao.deleteAll() } }
@@ -23,10 +25,4 @@ class ElementRepository(context: Context) {
         get() = LivePagedListBuilder(
             mElementDao.pagingSource(), 15
         ).build()
-
-    init {
-        val db: ElementRoomDatabase = ElementRoomDatabase.getInstance(context)
-        mElementDao = db.elementDao()
-        allElements = mElementDao.all
-    }
 }
