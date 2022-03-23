f9e2383ede1878bbe5fd1e31d5497e90ad92ea9b
rjbx <bob@coded.art>
2022-03-23 19:15:02 -0400

    Add testable database and mock repository to ListViewModelTest

--- app/src/test/java/art/coded/wireframe/ListViewModelTest.kt
+++ app/src/test/java/art/coded/wireframe/ListViewModelTest.kt
@@ -3,13 +3,14 @@ package art.coded.wireframe
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import art.coded.wireframe.model.ElementRepository
 import art.coded.wireframe.model.entity.Element
-import art.coded.wireframe.model.local.ElementDao
 import art.coded.wireframe.model.local.ElementRoomDatabase
 import art.coded.wireframe.viewmodel.ListViewModel
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -18,22 +19,32 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
+import java.io.IOException
 
 // adapted from raywenderlich.com
 
-@RunWith(AndroidJUnit4::class) class ListViewModelTest {
+@RunWith(AndroidJUnit4::class) open class ListViewModelTest {
 
+    private lateinit var db: ElementRoomDatabase
+    @Mock private lateinit var repository: ElementRepository
     @Mock private lateinit var viewModel: ListViewModel
     @Mock private lateinit var testActionPending: LiveData<Boolean>
     @Mock private lateinit var observer: Observer<Boolean>
 
     @get:Rule var instantExecutorRule = InstantTaskExecutorRule()
 
+    @Before fun initDb() {
+        db = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            ElementRoomDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+    }
+
     @Before fun setup() {
         MockitoAnnotations.initMocks(this)
-        val dao: ElementDao = ElementRoomDatabase.getInstance(
-            ApplicationProvider.getApplicationContext()).elementDao()
-        viewModel = spy(ListViewModel(ElementRepository(dao)))
+        repository = spy(ElementRepository(db.elementDao()))
+        viewModel = spy(ListViewModel(repository))
         testActionPending = viewModel.actionPending
     }
 
@@ -54,5 +65,11 @@ import org.mockito.MockitoAnnotations
         isPending = testActionPending.value
         assertNotNull(isPending)
         isPending?.let { assertFalse(it) }
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
     }
 }
