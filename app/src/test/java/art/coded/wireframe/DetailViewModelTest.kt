a5b6184be344a741dcfb1f7958050a06f8494664
rjbx <bob@coded.art>
2022-03-24 11:32:09 -0400

    Add tests and refactor database implementation for DetailViewModelTest

--- app/src/test/java/art/coded/wireframe/DetailViewModelTest.kt
+++ app/src/test/java/art/coded/wireframe/DetailViewModelTest.kt
@@ -26,7 +26,7 @@ import java.io.IOException
 
 @RunWith(AndroidJUnit4::class) open class DetailViewModelTest {
 
-    private lateinit var db: ElementRoomDatabase
+    @Mock private lateinit var db: ElementRoomDatabase
     @Mock private lateinit var repository: ElementRepository
     @Mock private lateinit var viewModel: DetailViewModel
     @Mock private lateinit var testActionPending: LiveData<Boolean>
@@ -35,40 +35,34 @@ import java.io.IOException
     @get:Rule var instantExecutorRule = InstantTaskExecutorRule()
 
     @Before fun setup() {
-
-        db = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
-            ElementRoomDatabase::class.java)
-            .allowMainThreadQueries()
-            .build()
-
         MockitoAnnotations.openMocks(this)
+        db = spy(ElementRoomDatabase.getInstance(ApplicationProvider.getApplicationContext()))
         repository = spy(ElementRepository(db.elementDao()))
         viewModel = spy(DetailViewModel(repository))
         testActionPending = viewModel.actionPending
     }
 
-//    @Test
-//    fun assertElementAdditionTest() {
-//        val testElement = Element(name = "Test Name", id = "10")
-//        var isPending = testActionPending.value
+    @Test
+    fun assertElementAdditionTest() {
+        val testElement = Element(name = "Test Name", id = "10")
+        var isPending = testActionPending.value
 //        assertNotNull(isPending)
-//        isPending?.let { assertTrue(it) }
-//        viewModel.addData(testElement)
-//        isPending = testActionPending.value
-//        assertNotNull(isPending)
-//        isPending?.let { assertFalse(it) }
-//    }
+        isPending?.let { assertTrue(it) }
+        viewModel.addData(testElement)
+        isPending = testActionPending.value
+        assertNotNull(isPending)
+        isPending?.let { assertFalse(it) }
+    }
 
-//    @Test fun assertElementRemovalTest() {
-//        var isPending = testActionPending.value
+    @Test fun assertElementRemovalTest() {
+        var isPending = testActionPending.value
 //        assertNotNull(isPending)
-//        isPending?.let { assertTrue(it) }
-//        viewModel.removeAllData()
-//        isPending = testActionPending.value
-//        assertNotNull(isPending)
-//        isPending?.let { assertFalse(it) }
-//    }
+        isPending?.let { assertTrue(it) }
+        viewModel.removeAllData()
+        isPending = testActionPending.value
+        assertNotNull(isPending)
+        isPending?.let { assertFalse(it) }
+    }
 
     @After
     @Throws(IOException::class)
